852b0f77021369156614d6b7e910d9abdb871c85
cumulus <luck1306@naver.com>
2023-07-04 11:20:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# deama-server
---
+++ src/main/java/com/example/deamaserver/DeamaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.deamaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeamaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeamaServerApplication.class, args);
+    }
+
+}
